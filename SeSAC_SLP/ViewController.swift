84464cc4c5010112a9fc99c25de2d32f9a68b612
sunnyou22 <sunnyouyaya22@gmail.com>
2022-11-07 17:48:00 +0900

    feat: 파베다는 중

--- SeSAC_SLP/ViewController.swift
+++ SeSAC_SLP/ViewController.swift
@@ -7,13 +7,26 @@
 
 import UIKit
 
+import FirebaseAuth
+import Messages
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let verificationID = UserDefaults.standard.string(forKey: "authVerificationID") // 이 부분 이해하기
+        let phoneNumber = "571552104"
+        Auth.auth().languageCode = "kr"
+        PhoneAuthProvider.provider()
+            .verifyPhoneNumber("+82 \(phoneNumber)", uiDelegate: nil) { (verificationID, error) in
+                if let id = verificationID {
+                    UserDefaults.standard.set("\(id)", forKey: "authVerificationID")
+                }
+                if let error = error {
+                    print(error.localizedDescription)
+                    return
+                }
+            }
     }
-
-
 }
 
